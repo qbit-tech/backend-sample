9d217a396d6a31594121f52ddab32b0a77dc96a0
muafa7 <muafammm@gmail.com>
2023-07-21 10:23:47 +0700

    fix

--- apps/base/src/core/output.interceptor.ts
+++ apps/base/src/core/output.interceptor.ts
@@ -1,13 +1,21 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { map } from "rxjs";
 
 export class OutputInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler): Promise<any> {
-    const payload = await next.handle().toPromise();
-    return {
-      code: 'success',
-      message: '',
-      payload
-    }
+    // const payload = await next.handle().pipe(map(data => ({ data })));
+    // return {
+    //   code: 'success',
+    //   message: '',
+    //   payload
+    // }
+    return next.handle().pipe(
+      map((payload) => ({
+        code: 'success',
+        message: '',
+        payload,
+      }))
+    ) 
   }
 }
 
