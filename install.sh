75ed95a928bbbd663ae027d9f679250aa5e78632
Aditia Prasetio <aditia.prasetio@qbit.com>
2023-08-16 14:36:09 +0700

    adjust version

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ yarn install
 
 echo "> INSTALL LIBS"
 yarn add qbit-tech/libs-notification
-yarn add qbit-tech/libs-utils
 yarn add qbit-tech/libs-authv3
 yarn add qbit-tech/libs-role
 yarn add qbit-tech/libs-uploader
 yarn add qbit-tech/libs-transaction
 yarn add qbit-tech/libs-faq
+yarn add qbit-tech/feature-utils
 
 echo "> RUN MIGRATION"
 ./run_migration.sh base local
