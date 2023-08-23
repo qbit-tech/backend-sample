7fcca4371f647482170d1277912f0f3a0552e668
Aditia Prasetio <aditia.prasetio@qbit.com>
2023-08-23 21:04:13 +0700

    adjust script

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ echo "> INSTALL ROOT"
 yarn install
 
 echo "> INSTALL LIBS"
-yarn add qbit-tech/libs-notification@latest
-yarn add qbit-tech/libs-authv3@latest
-yarn add qbit-tech/libs-role@latest
-yarn add qbit-tech/libs-uploader@latest
-yarn add qbit-tech/libs-transaction@latest
-yarn add qbit-tech/libs-faq@latest
-yarn add qbit-tech/libs-payments@latest
-yarn add qbit-tech/libs-products@latest
-yarn add qbit-tech/feature-utils@latest
+# yarn add qbit-tech/libs-notification#latest
+# yarn add qbit-tech/libs-authv3#latest
+yarn add qbit-tech/libs-role#latest
+yarn add qbit-tech/libs-uploader#latest
+yarn add qbit-tech/libs-transaction#latest
+yarn add qbit-tech/libs-faq#latest
+yarn add qbit-tech/libs-payments#latest
+yarn add qbit-tech/libs-products#latest
+yarn add qbit-tech/feature-utils#latest
 
 echo "> RUN MIGRATION"
 ./run_migration.sh base local
