fedfeb9aeb5f268710aa45c77b8fae9e3a28dfa1
Aditia Prasetio <aditia.prasetio@qbit.com>
2023-11-20 15:36:22 +0700

    remove pdf2html

--- apps/base/src/modules/ebook/ebook.controller.ts
+++ apps/base/src/modules/ebook/ebook.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, HttpException, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { getErrorStatusCode } from '@qbit-tech/libs-utils';
 import { EBOOKS, IEbook } from './ebook.data';
+// import * as pdf2html from 'pdf2html';
 
 @ApiTags('Ebook')
 @Controller('ebooks')
@@ -19,4 +20,19 @@ export class EbookController {
       throw new HttpException(err, getErrorStatusCode(err));
     }
   }
+
+  // @Get('pdf-to-html')
+  // async pdfToHTML(): Promise<{ results: IEbook[] }> {
+  //   try {
+  //     const html = await pdf2html.html(
+  //       '/Users/user/Documents/projects/backend-sample-new/apps/base/src/modules/ebook/file-example_PDF_1MB.pdf',
+  //     );
+  //     console.log(html);
+
+  //     return { results: html };
+  //   } catch (err) {
+  //     this.logger.error(err);
+  //     throw new HttpException(err, getErrorStatusCode(err));
+  //   }
+  // }
 }
