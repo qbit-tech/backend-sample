118fa92948288f7353875e57c0d4ed1b1d496033
Aditia Prasetio <aditia.prasetio12@gmail.com>
2023-12-26 14:25:07 +0700

    add install_only

--- install.sh
+++ install.sh
@@ -4,23 +4,7 @@ echo "==== START - Install ===="
 # echo "> PULL SUBMODULES"
 # git submodule update --init --recursive
 
-echo "> INSTALL ROOT"
-yarn install
-
-echo "> INSTALL LIBS"
-yarn add @qbit-tech/feature-utils
-yarn add @qbit-tech/libs-address
-yarn add @qbit-tech/libs-authv3
-yarn add @qbit-tech/libs-faq
-yarn add @qbit-tech/libs-notification
-yarn add @qbit-tech/libs-notification-scheduler
-yarn add @qbit-tech/libs-products
-yarn add @qbit-tech/libs-role
-yarn add @qbit-tech/libs-session
-yarn add @qbit-tech/libs-transaction
-yarn add @qbit-tech/libs-payments
-yarn add @qbit-tech/libs-uploader
-yarn add @qbit-tech/libs-utils
+./install_only.sh
 
 echo "> RUN MIGRATION"
 ./run_migration.sh base local
