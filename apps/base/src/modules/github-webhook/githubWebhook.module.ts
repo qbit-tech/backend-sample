04349a6af4283e365d0996593632869048e599c6
Aditia Prasetio <aditia.prasetio12@gmail.com>
2023-12-29 09:56:59 +0700

    add github webhook

---
+++ apps/base/src/modules/github-webhook/githubWebhook.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GithubWebhookController } from './githubWebhook.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV_PATH,
+    }),
+  ],
+  providers: [],
+  controllers: [
+    GithubWebhookController,
+  ],
+  exports: [
+    GithubWebhookService,
+    // EventGithubWebhookService
+  ]
+})
+
+export class GithubWebhookModule { }
