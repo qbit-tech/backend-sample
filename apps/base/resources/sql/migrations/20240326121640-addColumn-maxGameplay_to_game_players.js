d8e502888bbd8f17b18d20460392ff04d32f1096
Aditia Prasetio <aditia.prasetio12@gmail.com>
2024-03-26 12:17:59 +0700

    add column maxGameplay to game_players

---
+++ apps/base/resources/sql/migrations/20240326121640-addColumn-maxGameplay_to_game_players.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    return queryInterface.addColumn('game_players', 'maxGameplay', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    return queryInterface.removeColumn('game_players', 'maxGameplay');
+  },
+};
