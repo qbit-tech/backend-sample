326861adbbb4879d08cbda76c29ed6c1edab16a4
Aditia Prasetio <aditia.prasetio12@gmail.com>
2024-03-26 20:45:06 +0700

    fix whatsapp send message

--- apps/base/src/modules/whatsapp/whatsapp.controller.ts
+++ apps/base/src/modules/whatsapp/whatsapp.controller.ts
@@ -10,40 +10,42 @@ import {
   Post,
   Query,
   UseGuards,
+  HttpException,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { WhatsappService } from './whatsapp.service';
 import { WhatsappRequest } from './whatsapp.contract';
 import { WA_TO_NUMBER } from 'apps/base/config/whatsapp';
+import { getErrorStatusCode } from '@qbit-tech/libs-utils';
 
 @ApiTags('Whatsapp Business API')
 @Controller('/whatsapp')
-export class WhatsappController{
-  constructor(
-    private readonly whatsappService: WhatsappService,
-  ){}
+export class WhatsappController {
+  private readonly logger = new Logger(WhatsappController.name);
+  constructor(private readonly whatsappService: WhatsappService) {}
 
   @Post()
-  async reqSendMessage(
-    @Body() body: WhatsappRequest,
-  ):Promise<any>{
-    try{
-      Logger.log("===WA CONTROLLER SEND===");
+  async reqSendMessage(@Body() body: WhatsappRequest): Promise<any> {
+    try {
+      Logger.log('===WA CONTROLLER SEND===');
       const placeholder: WhatsappRequest = {
-        "messaging_product": "whatsapp",
-        "recipient_type": "individual",
-        "to": WA_TO_NUMBER,
-        "type": "text",
-        "text": {
-          "preview_url": false,
-          "body": "test",
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: WA_TO_NUMBER,
+        type: 'text',
+        text: {
+          preview_url: false,
+          body: 'test',
         },
-      }
-      const res = await this.whatsappService.sendMessage(placeholder);
-      Logger.log(JSON.stringify(res));
-    }catch(err){
-      Logger.log(JSON.stringify(err));
+      };
+      const resData = await this.whatsappService.sendMessage(placeholder);
+      this.logger.log('resData ' + JSON.stringify(resData));
+
+      return resData;
+    } catch (err) {
+      this.logger.error('ERROR reqSendMessage');
+      this.logger.error(err);
+      throw new HttpException(err, getErrorStatusCode(err));
     }
-    
   }
 }
--- apps/base/src/modules/whatsapp/whatsapp.service.ts
+++ apps/base/src/modules/whatsapp/whatsapp.service.ts
@@ -3,38 +3,49 @@ import { HttpService } from '@nestjs/axios';
 import { InjectModel } from '@nestjs/sequelize';
 import { Op } from 'sequelize';
 import { v4 as uuidv4 } from 'uuid';
-import { WA_ACCESS_TOKEN, WA_FROM_NUMBER, WA_TO_NUMBER } from 'apps/base/config/whatsapp';
+import {
+  WA_ACCESS_TOKEN,
+  WA_FROM_NUMBER,
+  WA_TO_NUMBER,
+} from 'apps/base/config/whatsapp';
 import { WhatsappRequest } from './whatsapp.contract';
 import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class WhatsappService{
-  constructor(
-    private httpService: HttpService,
-  ){}
+export class WhatsappService {
+  private readonly logger = new Logger(WhatsappService.name);
+  constructor(private httpService: HttpService) {}
 
-  async sendMessage(
-    body: WhatsappRequest,
-  ):Promise<any>{
-    try{
-      Logger.log("===WA SERVICE SEND===");
-      const res = await firstValueFrom(this.httpService.post('https://graph.facebook.com/v18.0/' + WA_FROM_NUMBER + '/messages', body, {
-        headers: {
-          'Authorization': 'Bearer ' + WA_ACCESS_TOKEN,
-          'Content-Type': 'application_json'
-        },
-      }
-      ).pipe(
-        catchError((error) => {
-          Logger.error(
-            'Error when request API'
-          );
-          throw error;
-        })
-      ));
-      Logger.log(JSON.stringify(res));
-    }catch(err){
-      Logger.error("err: " + JSON.stringify(err));
+  async sendMessage(body: WhatsappRequest): Promise<any> {
+    try {
+      Logger.log('===WA SERVICE SEND===');
+      const res = await firstValueFrom(
+        this.httpService
+          .post(
+            'https://graph.facebook.com/v18.0/' + WA_FROM_NUMBER + '/messages',
+            body,
+            {
+              headers: {
+                Authorization: 'Bearer ' + WA_ACCESS_TOKEN,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .pipe(
+            catchError((error) => {
+              Logger.error('Error when request API');
+              throw error;
+            }),
+          ),
+      );
+      this.logger.log('sendMessage res');
+      this.logger.log(res.data);
+
+      return res.data;
+    } catch (err) {
+      this.logger.error('err sendMessage');
+      this.logger.error(err);
+      return Promise.reject(err);
     }
   }
 }
